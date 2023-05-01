75e497df3970d1d6a0858b72f4e809270b8883ec
gauravgupta <gaurav.mobilefirst@gmail.com>
2023-05-01 13:11:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const timeline = gsap.timeline();
+timeline.to(".animate",{
+    delay:3,
+    duration:0.5,
+    opacity:0,
+})
+
+timeline.to(".animation",{
+    delay:1,
+    duration:1,
+    y:"100%",
+    ease:"power4.out"
+});
+
+timeline.to(".animation",{
+    zIndex:-1
+})
+
+timeline.from(".container h1", {
+    delay:0.5,
+    duration:0.8,
+    skewY:10,
+    y:100,
+    x:-199,
+    opacity:0,
+})
